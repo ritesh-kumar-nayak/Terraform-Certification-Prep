c7b1e6759bc6f3a7d7003bc8be04227e563835b4
RiteshKumarNayak <riteshnayakgudu49@gmail.com>
2024-04-02 08:48:38 +0530

    Demonstrated Datasources

---
+++ terraform-Datasources/output.tf
@@ -0,0 +1,13 @@
+output "instance_arn" {
+  value       = aws_instance.ec2-from-datasource[0].arn
+  description = "This is EC2 ARN"
+
+}
+
+output "ec2_public_ip" {
+  value       = aws_instance.ec2-from-datasource[*].public_ip # As we are using count meta argument for multiple EC2 instances, here we have to access the instance using it's index. To access all the instanc we are using [*]
+  description = "Public IP of instance"
+
+
+}
+
